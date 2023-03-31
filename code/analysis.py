b203d70bdfabd09aa724322b04f162d8d24a0696
jaygao1219 <jaygao1219@gmail.com>
2023-03-31 10:10:22 +0800

    [modify] 数据预处理

--- code/analysis.py
+++ code/analysis.py
@@ -6,13 +6,23 @@ import pandas as pd
 def generate_report(input_path, output_path):
     # Read in the data
     df = pd.read_excel(input_path, sheet_name='Sheet1')
+    df = df.astype(float)
     # Generate the report
     profile = ProfileReport(df, title='Pandas Profiling Report')
     # Save the report
     # profile.to_file(output_file='reports/pandas_profiling_report.html')
     profile.to_file(output_file=output_path)
 
+def data_clean(input_path):
+    # Read in the data
+    df = pd.read_excel(input_path, sheet_name='Sheet1')
+    df = df.astype(float)
+    # 打印每一列的数据类型
+    print(df.dtypes)
+    
+
 if __name__=='__main__':
     input_path= '../data/output.xlsx'
-    output_path = '../report/pandas_profiling_report.html'
-    generate_report(input_path, output_path)
+    # output_path = '../report/pandas_profiling_report.html'
+    # generate_report(input_path, output_path)
+    data_clean(input_path)
--- code/preprocess.py
+++ code/preprocess.py
@@ -4,27 +4,46 @@ def process_excel(input_path, output_path):
     # 打开 Excel 文件
     workbook = openpyxl.load_workbook(input_path)
 
+    # 选择工作表
+    worksheet = workbook.active
+
     # 新建一个 Excel 文件
     output_workbook = openpyxl.Workbook()
     output_worksheet = output_workbook.active
+    iid=[]
 
-    # 遍历每个工作表
-    # 把两个sheet合成一个sheet
-    for worksheet in workbook:
-        # 遍历每一行
-        for row in worksheet.iter_rows(values_only=True):
-            # 如果只有最后一列是 **，则赋值为倒数第二列的值
-            if row[-1] == '**' and row[-2] != '**':
-                row = list(row)
-                row[-1] = row[-2]
-            # 如果超过两列是 **，则忽略掉这一行
-            elif row.count('**') > 1:
+    # 遍历每一行
+    for row in worksheet.iter_rows(values_only=True):
+        row = list(row)
+        if row[0] in iid:
+            row[0] = -row[0]
+        # 如果只有最后一列是 **，则赋值为倒数第二列的值
+        if row[-1] == '**' and row[-2] != '**':
+            row = list(row)
+            row[-1] = row[-2]
+        # 如果非边界值是 **，则进行插值
+        elif '**' in row[1:-1]:
+            try:
+                for i in range(1, len(row)-1):
+                    if row[i] == '**':
+                        j = i - 1
+                        while row[j] == '**':
+                            j -= 1
+                        k = i + 1
+                        while row[k] == '**':
+                            k += 1
+                        row[i] = (row[j] + row[k]) / 2
+            except IndexError:
                 continue
-            # 写入新的 Excel 文件
-            output_worksheet.append(row)
+        # 写入新的 Excel 文件
+        iid.append(row[0])
+        row = tuple(row)
+        output_worksheet.append(row)
 
     # 保存并关闭文件
     output_workbook.save(output_path)
+
+
 
 # 测试代码
 if __name__=='__main__':
